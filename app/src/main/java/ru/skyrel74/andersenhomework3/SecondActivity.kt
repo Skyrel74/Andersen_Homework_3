dfc66aeaac2e3da4a9b6be3639b289197ba61fcd
Ilya Rakipov <ilya.rakipov@gmail.com>
2021-10-09 02:25:10 +0500

    feat: added glide load image implementation

--- app/src/main/java/ru/skyrel74/andersenhomework3/SecondActivity.kt
+++ app/src/main/java/ru/skyrel74/andersenhomework3/SecondActivity.kt
@@ -1,16 +1,19 @@
 package ru.skyrel74.andersenhomework3
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import ru.skyrel74.andersenhomework3.databinding.ActivitySecondBinding
 import java.net.URL
 
@@ -29,17 +32,44 @@ class SecondActivity : AppCompatActivity(R.layout.activity_second) {
         binding.editTextUrl.setOnEditorActionListener { textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE) {
 
-                val inputMethodManager =
-                    applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(binding.editTextUrl.windowToken, 0)
+                Glide.with(this)
+                    .asBitmap()
+                    .load(textView.text.toString())
+                    .listener(object : RequestListener<Bitmap> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Bitmap>?,
+                            isFirstResource: Boolean,
+                        ): Boolean {
+                            Toast.makeText(this@SecondActivity,
+                                "Не получилось загрузить изображение",
+                                Toast.LENGTH_SHORT).show()
+                            return false
+                        }
 
-                val urlString = textView.text.toString()
-                val image = RemoteImageManager().execute(urlString).get()
-                if (image != null)
-                    binding.imageView.setImageBitmap(image)
-                else
-                    Toast.makeText(this, "Не получилось загрузить изображение", Toast.LENGTH_SHORT)
-                        .show()
+                        override fun onResourceReady(
+                            resource: Bitmap?,
+                            model: Any?,
+                            target: Target<Bitmap>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean,
+                        ): Boolean = false
+
+                    })
+                    .into(binding.imageView)
+
+//                val inputMethodManager =
+//                    applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//                inputMethodManager.hideSoftInputFromWindow(binding.editTextUrl.windowToken, 0)
+//
+//                val urlString = textView.text.toString()
+//                val image = RemoteImageManager().execute(urlString).get()
+//                if (image != null)
+//                    binding.imageView.setImageBitmap(image)
+//                else
+//                    Toast.makeText(this, "Не получилось загрузить изображение", Toast.LENGTH_SHORT)
+//                        .show()
 
                 return@setOnEditorActionListener true
             }
